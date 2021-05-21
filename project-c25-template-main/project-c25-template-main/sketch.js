b1a8cff5c4fdab98410c61c4d6fe00fa6fe3570d
aeaeaeeee <75509200+aeaeaeeee@users.noreply.github.com>
2021-05-21 16:21:04 -0700

    Add files via upload

---
+++ project-c25-template-main/project-c25-template-main/sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+
+function preload()
+{
+}
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paperobj = new paper(300, 650, 50, 50);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  paperobj.display();
+  dustbinObj.display();
+
+}
+
+function keyPressed()
+{
+if(keyCode === UP_ARROW)
+{
+	Matter.Body.applyForce(paperobj.body, paperobj.body.position,{x:50, y:-50});
+}
+}
